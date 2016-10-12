cb52f83f51bbdaa0f32233b1b19589149d912962
Elena Uncu <uncu.elena@gmail.com>
2016-10-11 20:20:25 -0400

    Elena_LedZepp

---
+++ plugin/js/myplaylist.js
@@ -0,0 +1,49 @@
+var myPlaylist = [
+
+    {
+        mp3:'mix/1.mp3',
+        oga:'mix/1.ogg',
+        title:'Ramble on',
+        artist:'Led Zeppelin',
+        rating:5, 
+        duration:'4:35',
+        cover:'mix/1.png'
+		
+	},
+	{
+        mp3:'mix/2.mp3',
+        oga:'mix/1.ogg',
+        title:'Gallows Pole',
+        artist:'Led Zeppelin',
+        rating:5, 
+        duration:'4:55',
+        cover:'mix/2.png'
+	},
+	{
+		mp3:'mix/3.mp3',
+        oga:'mix/1.ogg',
+        title:'Whole lotta love',
+        artist:'Led Zeppelin',
+        rating:5, 
+        duration:'6:13',
+        cover:'mix/3.png'
+	},
+	{
+		mp3:'mix/4.mp3',
+        oga:'mix/1.ogg',
+        title:'Stairway To Heaven',
+        artist:'Led Zeppelin',
+        rating:5, 
+        duration:'7:59',
+        cover:'mix/4.png'
+	},
+	{
+		mp3:'mix/5.mp3',
+        oga:'mix/1.ogg',
+        title:'I am gonna crawl',
+        artist:'Led Zeppelin',
+        rating:5, 
+        duration:'5:30',
+        cover:'mix/5.png'
+	}
+];
